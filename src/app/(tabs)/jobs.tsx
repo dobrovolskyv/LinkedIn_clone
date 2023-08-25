5d3c23704a3264713662be85d2ed77f661901aaf
Vlados <malak3@mail.com>
2023-08-25 06:10:44 +0400

    add stepzen

--- src/app/(tabs)/jobs.tsx
+++ src/app/(tabs)/jobs.tsx
@@ -4,7 +4,7 @@ import { Text, View } from '@/components/Themed';
 export default function JobsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Jobs Screen</Text>
+      <Text style={styles.title}>Jobs Screens!</Text>
     </View>
   );
 }
